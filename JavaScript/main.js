38625abb1d227f607a71ebfc72496f7b72dde4fb
Foxify52 <93794624+Foxify52@users.noreply.github.com>
2022-06-22 10:31:01 -0400

    Add files via upload

---
+++ JavaScript/main.js
@@ -0,0 +1,19 @@
+if ("ontouchstart" in document.documentElement) {
+    document.documentElement.className += "no-touch";
+}
+window.onload = function () {
+    if ("ontouchstart" in document.documentElement) {
+        document.documentElement.className += "no-touch";
+    }
+    const menuBtn = document.querySelector('.checkbtn-main');
+    let menuOpen = false;
+    menuBtn.addEventListener('click', () => {
+        if (!menuOpen) {
+            menuBtn.classList.add('open');
+            menuOpen = true;
+        } else {
+            menuBtn.classList.remove('open');
+            menuOpen = false;
+        }
+    });
+}
